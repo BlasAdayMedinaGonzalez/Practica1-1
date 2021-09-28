3c221626f4825b57f30844015d5f2357840f0db1
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2021-09-28 11:15:38 +0100

    Se crea un try_catch para controlar errores y se añade código que vaya leyendo línea por línea
    Se crea un bucle para contar las filas y se va añadiendo cada nombre aleatorio en un arraylist
    
    con un bucle for se genera numero aleatorios y se va guardando en un array,
    en otro bucle se guarda el total de cada nota generado aleatoriamente.
    
    El total de cada nota se mete en un array y para luego hacer media
    
    Se imprime en consola cada línea

--- src/LectoryEscritura.java
+++ src/LectoryEscritura.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-
 
 
 class LectoryEscritura {
@@ -19,6 +20,7 @@ class LectoryEscritura {
     static double[] notaTotalArray = new double[LIMITE_PERSONAS];
     static int[] contadorRandomNombre = new int[LIMITE_PERSONAS];
     static Double[] Media = new Double[LIMITE_PERSONAS];
+
 
     public String getUrl() {
         return Url;
@@ -48,16 +50,60 @@ class LectoryEscritura {
         File file = null;
         BufferedReader bfR = null;
         file = new File(getUrl());
-
+        String linea = "";
         if (!file.exists()) {
             return "El fichero de entrada no existe";
         }
 
-        return "";
+        try {
+            bfR = new BufferedReader(new FileReader(file));
+            if ((linea = bfR.readLine()) == null) {
+                return "El fichero esta vacío";
+            }
+            // Se salta primera línea que no es nombre
+            if ((linea = bfR.readLine()) == null) {
+            }
+            // Se cuenta cada fila y se añade cada nombre en un array
+            while ((linea = bfR.readLine()) != null) {
+                contadorFila++;
+                Nombre.add(linea);
+            }
+
+            for (int i = 0; i < LIMITE_PERSONAS; i++) {
+
+                contadorRandomNombre[i] = (int) Math.floor(Math.random() * (0 - (contadorFila + 1)) + (contadorFila));
+                notaTotal = 0;
+                // bucle que guarda cada numero aleatorio y los asuma al total
+                for (int j = 0; j < nota.length; j++) {
+                    nota[j] = Math.round((Math.random() * 11) * 100.0) / 100.0;
+                    notaTotal += nota[j];
+                }
+                // Se guarda cada nota total en un array
+                notaTotalArray[i] = notaTotal;
+                // Se hace la media de cada nota
+                Media[i] = Math.round((notaTotalArray[i] / 4) * 100.0) / 100.0;
+
+                System.out.println(Nombre.get(contadorRandomNombre[i]) + " nota 1: " + nota[0] + " nota 2: " + nota[1]
+                        + " nota 3: " + nota[2] + " nota 4: " + nota[3] + " Media: "
+                        + Media[i]);
+
+            }
+            return "";
+        } catch (IOException e) {
+            return e.toString();
+        } finally {
+            try {
+                if (bfR != null)
+                    bfR.close();
+            } catch (Exception e) {
+                return "Error cerrando objetos:\n" + e;
+            }
+        }
     }
 
     public String pasaraFichero() {
-       return "";
+
+        return "";
     }
 
 }
